1f7c3200af19a5d63795a4dd4491abbb1449239b
Janaka Jati Lasmana <janakajati21@gmail.com>
2022-03-13 21:56:06 +0700

    Add current user story

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -3,8 +3,10 @@ import { ContextualCard } from '@faker-js/faker/helpers'
 import faker from '@faker-js/faker'
 
 import Story from './Story'
+import { useSession } from 'next-auth/react'
 
 function Stories() {
+  const { data: session } = useSession()
   const [suggestions, setSuggestions] = useState<ContextualCard[]>([])
 
   useEffect(() => {
@@ -15,7 +17,10 @@ function Stories() {
   }, [])
 
   return (
-    <div className="mt-8 flex space-x-2 overflow-x-scroll rounded-sm border border-gray-20 bg-white p-6 scrollbar-thin scrollbar-thumb-black">
+    <div className="border-gray-20 mt-8 flex space-x-2 overflow-x-scroll rounded-sm border bg-white p-6 scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <Story img={session.user.image!} username={session.user.username} />
+      )}
       {suggestions.map((item, index) => (
         <Story key={index} img={item.avatar} username={item.username} />
       ))}
